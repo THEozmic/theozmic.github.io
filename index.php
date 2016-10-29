5913259d8b7623c29b5ab66c178d9a63a2bdd52e
THEozmic <michaelozoemena2@gmail.com>
2016-10-29 19:18:09 +0100

    added index.php

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Oz</title>
+	<link rel="stylesheet" href="assets/css/style.css"/>
+	<script src="assets/js/jquery.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="utf-8">
+	<link href="assets/font/englebert.css" rel="stylesheet">
+	<link href="assets/font/hind.css" rel="stylesheet">
+	<link rel="stylesheet" href="assets/font-awesome-4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+<div id="sdbar">
+	<div id="rst-cls"></div>
+	<div id="mnu">
+		<div class="m-icon"><span class="fa fa-times"></span></div>
+		<div id="mnu-cnt"></div>
+	</div>
+</div>
+	<div id="tp">
+		<div id="tpbar"><div class="m-icon"><span class="fa fa-bars"></span></div></div>
+
+		<div id="bigtxt">
+		 Hi! I'm <span id="excl">Oz</span>
+		</div>
+		<div id="smtxt">Web Developer from Enugu, Nigeria.</div>
+	</div>
+	<div id="rst">
+			<div id="rst-fst">
+				<div class="btn-round" id="srlfrm"><span><img src="assets/img/down-arrow.png"></span></div>
+				<div id="srlto"></div>
+				<div id="cont">
+					<h4>Who Am I?</h4>
+					<p>My name is Michael <strong>Oz</strong>oemena. I love gorgeous websites.</p>
+					<h4>What I Do?</h4>
+					<p>Building stunnig websites and web apps is my life's second goal, with the first being living a happy life. I am a student at <a target="_blank" href="http://www.niit.com">NIIT</a>.</p>
+					<p>Skilled in Frontend, Backend and UI/UX design and development.</p>
+					<h4>What I've Done?</h4>
+					<p>Here's what I've been up to</p>
+				</div>
+				</div>
+				<div class="skew"></div>
+				<div id="prtf">
+						<div class="prtf-i">
+							<div class="img-cs"><img src="assets/img/prtfo/fornasetti.jpg"></div>
+							<div class="txt-cs">Fornasetti</div>
+						</div>
+
+						<div class="prtf-i">
+							<div class="img-cs"><img src="assets/img/prtfo/fornasetti.jpg"></div>
+							<div class="txt-cs">Fornasetti</div>
+						</div>
+
+						<div class="prtf-i">
+							<div class="img-cs"><img src="assets/img/prtfo/fornasetti.jpg"></div>
+							<div class="txt-cs">Fornasetti</div>
+						</div>
+
+						<div class="prtf-i">
+							<div class="img-cs"><img src="assets/img/prtfo/fornasetti.jpg"></div>
+							<div class="txt-cs">Fornasetti</div>
+						</div>
+
+						<div class="prtf-i">
+							<div class="img-cs"><img src="assets/img/prtfo/fornasetti.jpg"></div>
+							<div class="txt-cs">Fornasetti</div>
+						</div>
+
+						<div class="prtf-i">
+							<div class="img-cs"><img src="assets/img/prtfo/fornasetti.jpg"></div>
+							<div class="txt-cs">Fornasetti</div>
+						</div>
+						</div>
+	</div>
+	<div id="srlup"><span class="fa fa-arrow-up"></span></div>
+	<script src="assets/js/scripts.js"></script>
+</body>
+</html>
