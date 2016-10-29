76a0aed882157687c370b426188830925362267f
THEozmic <michaelozoemena2@gmail.com>
2016-10-29 19:19:05 +0100

    added assets folder

---
+++ assets/js/scripts.js
@@ -0,0 +1,17 @@
+$(".m-icon, #rst-cls").click(function(){
+	$("#sdbar").fadeToggle();
+});
+$("#srlfrm").click(function() {
+    $("html, body").animate({ scrollTop: $("#srlto").offset().top }, 1000);
+});
+$("#srlup").click(function() {
+    $("html, body").animate({ scrollTop: $("#tp").offset().top }, 1000);
+});
+
+$(window).scroll(function(){
+		if ($(this).scrollTop() > 200) {
+			$('#srlup').fadeIn();
+		} else {
+			$('#srlup').fadeOut();
+		}
+	});
