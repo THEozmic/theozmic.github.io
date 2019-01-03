4b80076713ccb07c009a9ab0dbdb4e48bd25d72b
Michael Ozoemena <michaelozoemena2@gmail.com>
2019-01-03 16:14:49 +0100

    Add offline support

---
+++ sw.js
@@ -0,0 +1,50 @@
+importScripts('cache-polyfill.js');
+const staticCacheName = 'theozmic-cache-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      return cache.addAll([
+        '/',
+        'https://theozmic.github.io/',
+        '/index.html',
+        '/assets/css/style.css',
+        'https://fonts.googleapis.com/css?family=Muli|Karla'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => {
+          return cacheName.startsWith('theozmic-cache') &&
+            cacheName != staticCacheName;
+        }).map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.open(staticCacheName).then((cache) => {
+      return cache.match(event.request).then((response) => {
+        return response || fetch(event.request).then((response) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
